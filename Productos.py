0fa670c9b6d5f75138b32c4e7b3cc982cbe8ceec
Emmanuel <emmanueljc1601@gmail.com>
2023-03-02 23:05:03 -0600

    LLenado de lista de productos, creacion de tipos de productos con atributos heredados, lista de objetos con los atributos de la lista de productos

--- Productos.py
+++ Productos.py
@@ -1,38 +1,64 @@
 class Producto():
     '''La clase Productos crea y define los parametros de un producto.
     
-    Identifica el tipo (str) de un producto, entiendase esto como [bebida, refrescos,
-    snacks, comida, dulce], recibe su nombre (str), su marca (str), su precio [float],
+    Identifica el tipo (str) de un producto, entiendase esto como [agua, refresco, jugo,
+    snacks, botana, dulce], recibe su nombre (str), su marca (str), su precio [float],
     la cantidad de ese producto[int] y su clave (str).
 
     Crea diccionarios con las listas de los productos.
     '''
-    def __init__(self:object,tipo:str,nombre:str,marca:str,precio:float,cantidad:int,clave:str) -> None:
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
         self.tipo=tipo
         self.nombre=nombre
         self.marca=marca
         self.precio=precio
-        self.__cantidad=cantidad
+        self.cantidad=cantidad
         self.clave=clave
 
     def mostrarProducto(self):
         print(self.nombre)
 
-    #def sumarRecaudación(self):
+class Snack(Producto):
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
+        super().__init__(clave,nombre,marca,precio,cantidad,tipo)
 
+class Jugo(Producto):
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
+        super().__init__(clave,nombre,marca,precio,cantidad,tipo)
 
+class Refresco(Producto):
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
+        super().__init__(clave,nombre,marca,precio,cantidad,tipo)
 
+class Agua(Producto):
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
+        super().__init__(clave,nombre,marca,precio,cantidad,tipo)
 
-# diccionario={}
-# listaAux=[]
+class Dulce(Producto):
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
+        super().__init__(clave,nombre,marca,precio,cantidad,tipo)
 
-# with open('prueba.csv','r') as f:
-#     contenido=f.readlines()
-#     for linea in contenido:
-#         palabras=linea.split(',')
-#         listaAux=palabras[:len(palabras)] #':' indica limites
-#         diccionario[listaAux[0]]=listaAux[1:len(listaAux)]
-#     print(diccionario)
+class Botana(Producto):
+    def __init__(self:object,clave:str,nombre:str,marca:str,precio:float,cantidad:int,tipo:str) -> None:
+        super().__init__(clave,nombre,marca,precio,cantidad,tipo)
 
-#with open('prueba2.csv','w+') as f:
-    #f.write('Hola Mundo\ndesde python')
+#Parte del código que me permite leer cada atributo de mis productos desde un csv
+#separa los atributos y los guarda en una lista que instancio con una clase
+listAux=[]
+listaProductos=[]
+with open('listasProductos.csv','r') as f:
+    datosProductos=f.readlines()
+    for linea in datosProductos:
+        atributos=linea.split(',')
+        listAux=atributos[:len(atributos)]
+        print(listAux)
+        #Realizo una lista de clases instanciadas->objetos
+        nombreClases = listAux[5].rstrip('\n')
+        listaProductos.append(globals()[nombreClases](listAux[0], listAux[1], listAux[2], listAux[3],listAux[4],listAux[5]))
+    #print(listaProductos) #Parece que imprime la lista de objetos, pero pone la memoria y no sus atributos
+
+#Tal cual un objeto puedo llamar a sus atributos de la lista y operar con él y sus métodos.
+#Ejemplo:
+print(listaProductos[1].nombre)
+
+#Posibles errores, que pongan un enter o les falte un dato en la lista de productos, tenerlo en cuenta, o se pasen
